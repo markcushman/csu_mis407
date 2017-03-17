984846d83379fa9180a5571c9a5ae3f15945662b
markcushman <mark@cushman.net>
2017-03-16 21:46:24 -0400

    changed trigger logic

--- mis-407_module5_createTriggers.sql
+++ mis-407_module5_createTriggers.sql
@@ -12,16 +12,23 @@ DROP TRIGGER IF EXISTS acme_crm.order_items_unit_cost_before_ins_tr;
 CREATE TRIGGER acme_crm.order_items_unit_cost_before_ins_tr BEFORE INSERT ON acme_crm.order_items
   FOR EACH ROW SET NEW.unit_cost = (SELECT unit_cost FROM acme_crm.items WHERE id = NEW.item_id);
 
-DROP TRIGGER IF EXISTS acme_crm.order_items_total_before_ins_tr;
-CREATE TRIGGER acme_crm.order_items_total_before_ins_tr BEFORE INSERT ON acme_crm.order_items
-  FOR EACH ROW SET NEW.total_cost = (SELECT unit_cost FROM acme_crm.items WHERE id = NEW.item_id) * NEW.quantity;
+DROP TRIGGER IF EXISTS acme_crm.order_items_total_after_ins_tr;
+CREATE TRIGGER acme_crm.order_items_total_after_ins_tr AFTER INSERT ON acme_crm.order_items
+  DELIMITER $$
+  CREATE TRIGGER acme_crm.order_total_after_ins AFTER INSERT on acme_crm.order_items
+    FOR EACH ROW BEGIN
+      UPDATE acme_crm.order_items
+        SET total_cost = (select sum(unit_cost) from acme_crm.order_items where id=NEW.id)
+        WHERE id = NEW.order_id;
+    END$$
+  DELIMITER ;
 
 DROP TRIGGER IF EXISTS acme_crm.order_total_after_ins;
 DELIMITER $$
 CREATE TRIGGER acme_crm.order_total_after_ins AFTER INSERT on acme_crm.order_items
   FOR EACH ROW BEGIN
     UPDATE acme_crm.orders
-      SET total_cost = (select sum(total_cost) from acme_crm.order_items where order_id=NEW.order_id)
+      SET order_total = (select sum(total_cost) from acme_crm.order_items where order_id=NEW.order_id)
       WHERE id = NEW.order_id;
   END$$
 DELIMITER ;
