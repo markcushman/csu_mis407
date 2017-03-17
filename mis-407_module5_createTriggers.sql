da43f2cf6899a6d705961912d99da2969394c97e
markcushman <mark@cushman.net>
2017-03-16 23:19:56 -0400

    Added comments to make code clearer

--- mis-407_module5_createTriggers.sql
+++ mis-407_module5_createTriggers.sql
@@ -8,8 +8,14 @@
 
 USE acme_crm;
 
+-- First drop the trigger
 DROP TRIGGER IF EXISTS acme_crm.order_items_cost_before_ins_tr;
+-- Change the delimiter to $$ so we can add multiple statements inside the
+-- create trigger logic
 DELIMITER $$
+-- Now create our trigger, the first step is to set the unit cost to a lookup
+-- from the unit_cost table
+-- Next step is to set the total_cost to the unit_cosst * quantity
 CREATE TRIGGER acme_crm.order_items_cost_before_ins_tr BEFORE INSERT ON acme_crm.order_items
 FOR EACH ROW BEGIN
   SET NEW.unit_cost = (SELECT unit_cost FROM acme_crm.items WHERE id = NEW.item_id);
@@ -17,8 +23,13 @@ FOR EACH ROW BEGIN
 END$$
 DELIMITER ;
 
+-- First drop the trigger
 DROP TRIGGER IF EXISTS acme_crm.order_total_after_ins;
+-- Change the delimiter to $$ so we can add multiple statements inside the
+-- create trigger logic
 DELIMITER $$
+-- We have to create a trigger that updates the order_total in the orders table
+-- from the sum of the order_items table
 CREATE TRIGGER acme_crm.order_total_after_ins AFTER INSERT ON acme_crm.order_items
   FOR EACH ROW BEGIN
     UPDATE acme_crm.orders
--- mis-407_module5_populateDB_customers.sql
+++ mis-407_module5_populateDB_customers.sql
@@ -20,4 +20,4 @@ INSERT INTO acme_crm.customers (first_name,last_name,address1,address2,
   ('Ringo','Starr','666 Water Street','Unit 5','New York','10001','NY','US'),
   ('Bugs','Bunny','1 Rabbit Hole',NULL,'Pizmo Beach','90211','CA','US'),
   ('Yosemite','Sam','100 Gold Rush Ave.','Unit 1','Yukon','99999','AK','US'),
-  ('Daffy','Duck','12 Bayou Lane',NULL,'Layfayette','40001','LA','US');
+  ('Daffy','Duck','12 Bayou Lane',NULL,'Layfayette','40001','LA','US'); 
