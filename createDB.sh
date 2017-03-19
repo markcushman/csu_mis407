4cb26cb3d85166316039a000d650d40246350fb7
markcushman <mark@cushman.net>
2017-03-19 00:10:10 -0400

    Added populating suppliers data to createDB

--- createDB.sh
+++ createDB.sh
@@ -7,6 +7,7 @@ rm -rf /tmp/sql.tmp
 cat mis-407_module5_createDB.sql > /tmp/sql.tmp
 cat mis-407_module5_createTriggers.sql >> /tmp/sql.tmp
 cat mis-407_module5_populateDB_customers.sql >> /tmp/sql.tmp
+cat mis-407_module5_populateDB_suppliers.sql >> /tmp/sql.tmp
 cat mis-407_module5_populateDB_items.sql >> /tmp/sql.tmp
 cat mis-407_module5_populateDB_orders.sql >> /tmp/sql.tmp
 cat mis-407_module5_populateDB_order_items.sql >> /tmp/sql.tmp
